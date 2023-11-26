1de6f95772ecc6d9aa3b5a23124bc1622ca1c94c
Steph <stazatorres@hotmail.com>
2023-11-26 10:56:13 +0000

    trign a differnet method

--- nextSmallestNumberSameDigits.js
+++ nextSmallestNumberSameDigits.js
@@ -1,29 +1,32 @@
 function nextSmaller(num) {
-  /* could take num, make an array create an array with values using the numbers in any order and return one below original number... */
   const arr = convertNumberToArray(num);
+  const stringNum = transformArrToSingleDigit(arr);
 
   const numbersArr = [];
   const combinations = permute(arr);
-  /* Transform set to array */
-  const combinationsArr = [...combinations];
 
   /* Loop over each combination transform each digit to a string so can concatenate together then transfer back to number to sort. */
 
-  combinationsArr.forEach((combo) => {
+  /* combinationsArr.forEach((combo) => {
     numbersArr.push(transformArrToSingleDigit(combo));
-  });
+  }); 
+  forEach is slower than for loop*/
+
+  for (let i = 0; i < combinations.length; i++) {
+    numbersArr.push(transformArrToSingleDigit(combinations[i]));
+  }
 
   sortedArr = numbersArr.sort((a, b) => a - b);
 
   for (let i = 0; i < sortedArr.length; i++) {
-    if (sortedArr[i] === num) {
+    if (sortedArr[i] === stringNum) {
       if (!sortedArr[i - 1]) {
         return -1;
       }
-      if (convertNumberToArray(sortedArr[i - 1]).length < arr.length) {
+      if (sortedArr[i - 1][0] === "0") {
         return -1;
       }
-      return sortedArr[i - 1];
+      return Number(sortedArr[i - 1]);
     }
   }
 }
@@ -50,13 +53,12 @@ function transformArrToSingleDigit(arr) {
     value = value.concat(arr[i]);
   }
 
-  return Number(value);
+  return value;
 }
 
 function convertNumberToArray(num) {
-  const arr = String(num)
-    .split("")
-    .map((char) => Number(char));
+  const arr = String(num).split("");
+  // .map((char) => Number(char));
   return arr;
 }
 
@@ -65,25 +67,77 @@ function convertNumberToArray(num) {
 /* maybe cycle backwards through the number. check if the last number is the smallest, if yes, switch, if not move on to the next 
  i.e tens, if 100's is smaller switch and so on.. double check both initial number and final number are teh same length */
 
-/* function messUpArr(arr) {
+/* function tryignAgain(num) {
+  const arr = convertNumberToArray(num);
   const dup = [...arr];
+  const firstNumber = dup.shift();
+  //return dup;
 
-  const result = [];
+  /* start at right 
+  for(let i = dup.length-1; i>= 0 ; i--){
+    for(let j= dup.length-1; j>=0; j--){
+        if()
 
-  
-  for (let i = arr.length - 1; i <= 0; i--) {
-    for (let j = arr.length - 2; j <= 0; j--) {
-      if (arr[i] < arr[j]) {
-        [...arr, arr[j], arr[i]] = [...arr, arr[i], arr[j]];
-      }
     }
   }
 
-  const firstNumber = dup.shift();
+  
   //return [firstNumber, dup]
 
   if (dup.every((num) => num > firstNumber)) {
+  } 
+}
+
+console.log(tryignAgain(231)); */
+
+/* DIFFERENT WAY, HAD TO WATCH A VIDEO FOR SOLUTION, 
+-FIND DIGIT WHERE THE FOLLWOING DIGIT IS LOWER,
+-REMOVE DIGIT, BUMPS LOWER DIGIT TO IT'S POSTION,
+-CONCAT FOLLOWING DIGITS IN DESCENDING ORDER,
+-CONCAT SPLIT DIGIT TO END */
+
+function getSplitDigitIndex(stringNum) {
+  /* we are starting from the right, 
+    start at second in from right because last digit/value has nothing after it to compare */
+  for (let i = stringNum.length - 2; i >= 0; i--) {
+    if (stringNum[i + 1] < stringNum[i]) {
+      return i;
+    }
   }
-} */
+  return -1;
+}
 
+function getSmaller(num) {
+  const stringNum = String(num);
 
+  const splitIndex = getSplitDigitIndex(stringNum);
+
+  const lastDigit = stringNum.slice(splitIndex, splitIndex + 1);
+  const front = stringNum.substring(0, splitIndex);
+  const end = stringNum.substring(splitIndex + 1);
+  const sortedEnd = transformArrToSingleDigit(
+    end.split("").sort((a, b) => b - a)
+  );
+
+  let result;
+
+  result = front + sortedEnd + lastDigit;
+
+  if (sortedEnd[sortedEnd.length - 1] === "0") {
+    result = front + sortedEnd.slice(0, -1) + lastDigit + "0";
+  }
+
+  if (result > stringNum) {
+    return -1;
+  }
+
+  if (result[0] === 0) {
+    return -1;
+  }
+
+  return Number(result);
+}
+
+console.log(getSmaller(907));
+console.log(getSmaller(414));
+console.log(getSmaller(1234567908));
